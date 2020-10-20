dbae6cf2622797866f15ac5239ffc44321e5cc5a
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-10-20 11:36:46 +0200

    shared_ptr exercise

---
+++ _exercise/shared_ptr_ex/wsl-build/CMakeFiles/shared_ptr_ex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shared_ptr_ex.dir/main.cpp.o"
+  "shared_ptr_ex.pdb"
+  "shared_ptr_ex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shared_ptr_ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _exercise/shared_ptr_ex/wsl-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/infotraining/code/cpp-11/code/_exercises/shared_ptr_ex
+# Build directory: /mnt/d/infotraining/code/cpp-11/code/_exercises/shared_ptr_ex/wsl-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "shared_ptr_ex")
